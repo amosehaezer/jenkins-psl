f105fb40c58d9dd3905442b6f9124eb2eae82f66
Amos Ebenhaezer <amosehaezer@gmail.com>
2021-04-30 20:54:08 +0700

    change some file

--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -7,7 +7,25 @@ import com.workshop.stages.*
 def main(script) {
     // Object initialization
 
+    c = new Config()
+
     // Pipeline object
+
+    def repository_name = ("${script.env.repository_name}" != "null") ? "${script.env.repository_name}" : ""
+    def branch_name = ("${script.env.branch_name}" != "null") ? "${script.env.branch_name}" : ""
+    def git_user = ("${script.env.git_user}" != "null") ? "${script.env.git_user}" : ""
+    def docker_user = ("${script.env.docker_user}" != null) ? "${script.env.docker_user}" : ""
+    def app_port = ("${script.env.app_port}" != null) ? "${script.env.app_port}" : ""
+    def pr_num = ("${script.env.pr_num}" != null) ? "${script.env.pr_num}" : ""
+
+    p = new Pipeline(
+        repository_name,
+        branch_name,
+        git_user,
+        docker_user,
+        app_port,
+        pr_num
+    )
 
     ansiColor('xterm') {
         stage('Test stage') {
